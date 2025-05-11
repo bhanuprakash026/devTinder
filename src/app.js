3e3f05c72e91c7e3e9e9c184a94dba767e53b2d6
bhanuprakash026 <bhanuprakash.t@perigorddata.com>
2025-05-11 22:13:19 +0530

    schema validations and added CRUD operations

--- src/app.js
+++ src/app.js
@@ -3,27 +3,66 @@ const app = express();
 const connectDB = require('./config/database')
 const User = require('./models/user')
 
-app.post('/signup', async (req, res) => {
-    const user = new User({
-        firstName: "John",
-        lastName: "Doe",
-        emailId: "john.doe@gmail.com",
-        age: 25,
-        gender: "male",
-        password: "John@123"
-    })
+app.use(express.json())
 
-    await user.save();
+app.post('/signup', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save(user);
     res.send("User added successfully!")
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+
+});
+
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("User not found!!")
+    } else {
+      res.send(user);
+    }
+
+  } catch (error) {
+    res.status(400).send(`Something went wrong ${error.message}`)
+  }
+})
+
+app.get("/feed", async (req, res) => {
+  const users = await User.find({});
+  try {
+    res.send(users)
+  } catch (error) {
+    res.send(`${error.message}`)
+  }
+});
+
+app.post("/user", async (req, res) => {
+  const { firstName, updateData } = req.body
+  try {
+    const updatedUser = await User.findOneAndUpdate({ firstName: firstName }, { $set: updateData }, { new: true });
+    if (updatedUser) {
+      res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+      console.log("updatedUser", updatedUser)
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+
+  } catch (error) {
+
+  }
 })
 
 connectDB()
-    .then(() => {
-        console.log('Database connection established Successfully');
-        app.listen(7777, () => {
-            console.log('Server is Running on PORT 7777')
-        })
+  .then(() => {
+    console.log('Database connection established Successfully');
+    app.listen(7777, () => {
+      console.log('Server is Running on PORT 7777')
     })
-    .catch((error) => {
-        console.log(error.message);
-    })
+  })
+  .catch((error) => {
+    console.log(error.message);
+  })
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,51 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
 	firstName: {
-		type: String
+		type: String,
+		required: true,
+		minLength: 4,
+		maxLength: 50,
+		trim: true,
 	},
 	lastName: {
-		type: String
+		type: String,
+		required: true,
+		minLength: 4,
+		maxLength: 50,
+		trim: true,
 	},
 	emailId: {
-		type: String
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true,
 	},
 	age: {
-		type: Number
+		type: Number,
+		required: true,
+		min: 18,
+		trim: true,
 	},
 	gender: {
-		type: String
+		type: String,
+		required: true,
+		validate(value) {
+			if(!["male", "female", "others"].includes(value)) {
+				throw new Error("Invalid Gender data")
+			}
+		},
+		trim: true
 	},
 	password: {
-		type: String
+		type: String,
+		required: true,
+		trim: true,
+	},
+	photo: {
+		type: String,
+		default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcampussafetyconference.com%2Fbusinessman-silhouette-as-avatar-or-default-profile-picture&psig=AOvVaw052_WlhwliZsXJq08Fz3Ha&ust=1747067950517000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjViMLtm40DFQAAAAAdAAAAABAE"
 	}
-})
+}, {timestamps: true})
 
 module.exports = mongoose.model("User", userSchema);
